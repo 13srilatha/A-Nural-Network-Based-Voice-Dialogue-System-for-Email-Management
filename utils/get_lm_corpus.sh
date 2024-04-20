abf50c626f021c3dfa1db77609578bc195a21c81
Srilatha <118241048+13srilatha@users.noreply.github.com>
2024-04-21 05:01:36 +0530

    Delete utils directory

--- utils/get_lm_corpus.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#wget http://qwone.com/~jason/20Newsgroups/20news-bydate.tar.gz
-#gzip -d 20news-bydate.tar.gz
-#sed -i'' -E 's/<[^<]+>//g' testcorpus.txt
-#split --bytes=200M testcorpus.txt
-mv xaa data/lm/corpus.txt
