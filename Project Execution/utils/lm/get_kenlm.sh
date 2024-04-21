55338b487c1481eaea21ebbf2422273b97bf3170
Srilatha <118241048+13srilatha@users.noreply.github.com>
2024-04-21 05:35:15 +0530

    Update get_kenlm.sh

--- Project Execution/utils/lm/get_kenlm.sh
+++ Project Execution/utils/lm/get_kenlm.sh
@@ -4,7 +4,3 @@ mkdir kenlm/build
 cd kenlm/build
 cmake ..
 make -j2
-#cd ..
-#python3 -m venv newv
-#source /mnt/c/Users/srila/OneDrive/Desktop/project2/blind-mailAI/newv/scripts/activate
-#python3 -m pip install -e .
